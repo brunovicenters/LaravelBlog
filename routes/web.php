a542092fe6a37736c0c30c81c12284a28eb0b118
Bruno <bruno@Roberta>
2023-10-19 14:17:48 -0300

    Added new route

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('blog');
+});
+
+
+Route::get('', function () {
+    return view('post');
 });
