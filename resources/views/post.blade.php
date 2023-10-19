267d5cbd552cbbedeb38515995d68510edacf21a
Bruno <bruno@Roberta>
2023-10-19 14:57:29 -0300

    First commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/styles/app.css">
+    <title>Post</title>
+</head>
+
+<body>
+    <article>
+        <?= $post; ?>
+    </article>
+    <a href="/">Go back</a>
+</body>
+
+</html>
