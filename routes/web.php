1bc6784b82887bd130ed92edbce7716bf64ef742
Bruno <bruno@Roberta>
2023-10-19 15:31:26 -0300

    Added cache to posts route

--- routes/web.php
+++ routes/web.php
@@ -19,14 +19,11 @@ Route::get('/', function () {
 
 
 Route::get('posts/{post}', function ($slug) {
-    $path = __DIR__ . "/../resources/posts/$slug.html";
-
-    if (file_exists($path)) {
-        $post = file_get_contents($path);
-        return view('post', [
-            'post' => $post,
-        ]);
-    } else {
+    if (!file_exists($path = __DIR__ . "/../resources/posts/$slug.html")) {
         return redirect('/');
     }
+
+    $post = cache()->remember("posts.{$slug}", now()->addMinutes(20), fn () => file_get_contents($path));
+
+    return view('post', ['post' => $post,]);
 })->where('slug', '[A-z_\-]+');
