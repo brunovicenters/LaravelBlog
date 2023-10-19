13845ef33ede925c34e2e3e6179472466e7cab67
Bruno <bruno@Roberta>
2023-10-19 14:17:06 -0300

    Added posts + Renamed

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/app.css">
+    <title>My blog</title>
+</head>
+
+<body>
+    <article>
+        <h1>My First Post</h1>
+
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus, molestiae nihil saepe error repudiandae ab praesentium tenetur itaque autem fuga. Veniam error non minima porro possimus, deserunt facere vel placeat?
+            Consequatur debitis consectetur sapiente pariatur labore eligendi unde officiis, ducimus inventore nam explicabo totam accusamus atque dignissimos quam maxime natus quod tempora! Iure quod aperiam explicabo voluptates eos facere provident!
+            Totam, deleniti. Provident, fugit, iste obcaecati enim fuga ut, alias quos placeat architecto accusantium laudantium in unde? Quidem libero dicta veniam accusantium nam, temporibus commodi asperiores sunt voluptatem officia quod!
+        </p>
+    </article>
+
+    <article>
+        <h1>My Second Post</h1>
+
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus, molestiae nihil saepe error repudiandae ab praesentium tenetur itaque autem fuga. Veniam error non minima porro possimus, deserunt facere vel placeat?
+            Consequatur debitis consectetur sapiente pariatur labore eligendi unde officiis, ducimus inventore nam explicabo totam accusamus atque dignissimos quam maxime natus quod tempora! Iure quod aperiam explicabo voluptates eos facere provident!
+            Totam, deleniti. Provident, fugit, iste obcaecati enim fuga ut, alias quos placeat architecto accusantium laudantium in unde? Quidem libero dicta veniam accusantium nam, temporibus commodi asperiores sunt voluptatem officia quod!
+        </p>
+    </article>
+
+    <article>
+        <h1>My Third Post</h1>
+
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus, molestiae nihil saepe error repudiandae ab praesentium tenetur itaque autem fuga. Veniam error non minima porro possimus, deserunt facere vel placeat?
+            Consequatur debitis consectetur sapiente pariatur labore eligendi unde officiis, ducimus inventore nam explicabo totam accusamus atque dignissimos quam maxime natus quod tempora! Iure quod aperiam explicabo voluptates eos facere provident!
+            Totam, deleniti. Provident, fugit, iste obcaecati enim fuga ut, alias quos placeat architecto accusantium laudantium in unde? Quidem libero dicta veniam accusantium nam, temporibus commodi asperiores sunt voluptatem officia quod!
+        </p>
+    </article>
+</body>
+
+</html>
