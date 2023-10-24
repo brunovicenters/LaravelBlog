a27aa2d7a2e7d36339b9f700ccf462670930d130
brunovicenters <brunovicente.dev@gmail.com>
2023-10-24 10:05:12 -0300

    Added banner layout

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,10 +1,14 @@
 @extends('layout')
 
+@section('banner')
+    <h1>My blog</h1>
+@endsection
+
 @section('content')
    @foreach ($posts as $post)
     <article class="{{ $loop->even ? 'foobar' : '' }}">
         <a href="/posts/{{$post->slug}}">
-            <h1> {{$post->title}} </h1>
+            <h2> {{$post->title}} </h2>
         </a>
         <div>{{$post->excerpt}}</div>
     </article>
