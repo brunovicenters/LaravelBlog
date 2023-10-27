9225f4069173771800509fb56bc74736f2256695
brunovicenters <brunovicente.dev@gmail.com>
2023-10-27 14:50:08 -0300

    Updated from slug to id

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
         @foreach ($posts as $post)
             <article class="{{ $loop->even ? 'foobar' : '' }}">
-                <a href="/posts/{{$post->slug}}">
+                <a href="/posts/{{$post->id}}">
                     <h2> {{$post->title}} </h2>
                 </a>
                 <div>{{$post->excerpt}}</div>
