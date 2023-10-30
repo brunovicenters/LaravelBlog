7f50ea2746ebbf24b6fe4d873780e9e28954fe4f
brunovicenters <brunovicente.dev@gmail.com>
2023-10-30 10:04:27 -0300

    Changed back to div

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
         <article>
             <h1>{{$post->title}}</h1>
-            <p> {!! $post->body !!} </p>
+            <div> {!! $post->body !!} </div>
         </article>
         <a href="/">Go back</a>
 </x-layout>
