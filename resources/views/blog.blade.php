22e6c719af412d132fad1e8ebadd1225ac78e49f
brunovicenters <brunovicente.dev@gmail.com>
2023-10-31 14:45:20 -0300

    Added category

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,10 +1,17 @@
 <x-layout>
         @foreach ($posts as $post)
             <article class="{{ $loop->even ? 'foobar' : '' }}">
-                <a href="/posts/{{$post->slug}}">
-                    <h2> {!!$post->title!!} </h2>
-                </a>
-                <div>{{$post->excerpt}}</div>
+                <h2> 
+                    <a href="/posts/{{$post->slug}}">{!!$post->title!!}</a>
+                </h2>
+                
+                <p>
+                    <a href="#">{{$post->category->name}}</a>
+                </p>
+
+                <div>
+                    {{$post->excerpt}}
+                </div>
             </article>
         @endforeach
 </x-layout>
