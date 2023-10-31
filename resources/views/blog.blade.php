fe7e2828dcf95027bb97e7d3c5432096db1aa4b0
brunovicenters <brunovicente.dev@gmail.com>
2023-10-31 10:38:09 -0300

    Added slug

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
         @foreach ($posts as $post)
             <article class="{{ $loop->even ? 'foobar' : '' }}">
-                <a href="/posts/{{$post->id}}">
+                <a href="/posts/{{$post->slug}}">
                     <h2> {!!$post->title!!} </h2>
                 </a>
                 <div>{{$post->excerpt}}</div>
