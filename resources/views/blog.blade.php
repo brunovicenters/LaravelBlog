8b2fad482f534c5e2cee10eac89d27b7adc96560
brunovicenters <brunovicente.dev@gmail.com>
2023-11-01 10:10:17 -0300

    Added filter by category

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -6,11 +6,11 @@
                 </h2>
                 
                 <p>
-                    <a href="#">{{$post->category->name}}</a>
+                    <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
                 </p>
 
                 <div>
-                    {{$post->excerpt}}
+                    {!!$post->excerpt!!}
                 </div>
             </article>
         @endforeach
