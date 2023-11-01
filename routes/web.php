b5dfa83d921a18a8ce6fbc7f5c1980761eb9f243
brunovicenters <brunovicente.dev@gmail.com>
2023-11-01 10:49:16 -0300

    Handling multiple sql queries

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('blog', ['posts' => Post::all()]);
+    return view('blog', ['posts' => Post::with('category')->get()]);
 });
 
 
