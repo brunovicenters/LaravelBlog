8400a0615c575d6cb14538c128d48da10eae3ab2
brunovicenters <brunovicente.dev@gmail.com>
2023-11-01 11:16:36 -0300

    Create basic seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use \App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +17,52 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
+
+        $user = User::factory()->create();
+
+
+        $personal = Category::create([
+            "name" => 'Personal',
+            "slug" => 'personal',
+        ]);
+
+        $work = Category::create([
+            "name" => 'Work',
+            "slug" => 'work',
+        ]);
+
+        $hobbies = Category::create([
+            "name" => 'Hobbies',
+            "slug" => 'hobbies',
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $personal->id,
+            'title' => 'My Family Post',
+            'slug' => 'my-family-post',
+            'excerpt' => '<p>Excerpt for my post</p>',
+            'body' => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati dolor officiis cumque, facere natus veritatis nostrum quo libero. Quasi, eius dignissimos accusamus quod ratione aperiam sed fuga quibusdam cum aliquid?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis eligendi expedita rem amet animi pariatur, consequuntur delectus aut! Voluptatem aperiam nisi, tempora eveniet libero itaque facere voluptas maxime magnam ab.Quod illo eaque laborum provident consectetur non commodi maxime, quam quidem voluptas! Fugit, molestiae corporis, fugiat eligendi labore quisquam optio culpa ipsa, deserunt nemo distinctio sequi porro incidunt provident neque.</p>',
+        ]);
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work Post',
+            'slug' => 'my-work-post',
+            'excerpt' => '<p>Excerpt for my post</p>',
+            'body' => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati dolor officiis cumque, facere natus veritatis nostrum quo libero. Quasi, eius dignissimos accusamus quod ratione aperiam sed fuga quibusdam cum aliquid?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis eligendi expedita rem amet animi pariatur, consequuntur delectus aut! Voluptatem aperiam nisi, tempora eveniet libero itaque facere voluptas maxime magnam ab.Quod illo eaque laborum provident consectetur non commodi maxime, quam quidem voluptas! Fugit, molestiae corporis, fugiat eligendi labore quisquam optio culpa ipsa, deserunt nemo distinctio sequi porro incidunt provident neque.</p>',
+        ]);
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $hobbies->id,
+            'title' => 'My Hobbies Post',
+            'slug' => 'my-hobbies-post',
+            'excerpt' => '<p>Excerpt for my post</p>',
+            'body' => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati dolor officiis cumque, facere natus veritatis nostrum quo libero. Quasi, eius dignissimos accusamus quod ratione aperiam sed fuga quibusdam cum aliquid?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis eligendi expedita rem amet animi pariatur, consequuntur delectus aut! Voluptatem aperiam nisi, tempora eveniet libero itaque facere voluptas maxime magnam ab.Quod illo eaque laborum provident consectetur non commodi maxime, quam quidem voluptas! Fugit, molestiae corporis, fugiat eligendi labore quisquam optio culpa ipsa, deserunt nemo distinctio sequi porro incidunt provident neque.</p>',
+        ]);
     }
 }
