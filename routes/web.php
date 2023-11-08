172041014f33dea236ba15ec7e0c615ec558e13c
brunovicenters <brunovicente.dev@gmail.com>
2023-11-08 14:36:44 -0300

    Deleted "with" (it's default on the model)

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('blog', ['posts' => Post::latest()->with('category', 'author')->get()]);
+    return view('blog', ['posts' => Post::latest()->get()]);
 });
 
 
