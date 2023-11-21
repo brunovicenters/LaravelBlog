fe94c9250a4601b6924e60574c06d1810276d4e0
brunovicenters <brunovicente.dev@gmail.com>
2023-11-21 11:38:56 -0300

    Dinamic

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -10,52 +10,41 @@
         <header>
           <div class="space-x-2">
             <a
-              href="#"
+              href="/categories/{{ $post->category->slug }}"
               class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 uppercase font-semibold"
               style="font-size: 10px">
-              Techniques
+              {{ $post->category->name}}
             </a>
-            <a
-              href="#"
-              class="px-3 py-1 border border-red-300 rounded-full text-red-300 uppercase font-semibold"
-              style="font-size: 10px">
-              Updates
-            </a>
+
           </div>
           <div class="mt-4">
             <h2 class="text-3xl">
-              This is a big title and it will look great on two or even
-              three lines. Wooohoo!
+              <a href="/posts/{{ $post->slug }}">
+                {{ $post->title }}
+              </a> 
             </h2>
             <span class="mt-2 block text-gray-400 text-xs">
               Published
-              <time>1 day ago</time>
+              <time>{{ $post->created_at->diffForHumans() }}</time>
             </span>
           </div>
         </header>
         <div class="text-sm mt-4">
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Mollitia impedit atque odio incidunt? Accusantium, expedita
-            modi perferendis rem repudiandae ipsa dolores non rerum
-            assumenda. Vitae similique quibusdam non nisi reprehenderit!
-          </p>
-          <p class="mt-4">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Similique ratione iure dolorum.
+            {{ $post->excerpt }}
           </p>
         </div>
         <footer class="flex justify-between items-center mt-8">
           <div class="flex items-center text-sm">
             <img src="/images/lary-avatar.svg" alt="Lary avatar" />
             <div class="ml-3">
-              <h5 class="font-bold">Lary Laracore</h5>
+              <h5 class="font-bold">{{ $post->author->name }}</h5>
               <h6>Mascot at Laracasts</h6>
             </div>
           </div>
           <div class="hidden lg:block">
             <a
-              href="#"
+              href="/posts/{{ $post->slug }}"
               class="text-sm font-semibold bg-gray-200 rounded-full py-2 px-8">
               Read More
             </a>
