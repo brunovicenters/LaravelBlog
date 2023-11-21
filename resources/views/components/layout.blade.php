9bd3084d1eb4bc37f28facb0d59010b408487499
brunovicenters <brunovicente.dev@gmail.com>
2023-11-21 09:41:29 -0300

    Added navbar and footer to template

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,17 +1,76 @@
 <!DOCTYPE html>
 <html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
+      rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Comic+Neue:wght@400;700&family=Open+Sans:wght@400;600;700&display=swap"
+      rel="stylesheet" />
+    <title>Laravel From Scratch Blog</title>
+  </head>
+  <body style="font-family: Open Sans, sans-serif">
+    <section class="px-6 py-8">
+      <nav class="md:flex md:justify-between md:items-center">
+        <div>
+          <a href="/">
+            <img
+              src="/images/logo.svg"
+              alt="Laracasts Logo"
+              width="165"
+              height="16" />
+          </a>
+        </div>
+        <div class="mt-8 lg:mt-0">
+          <a href="/" class="text-xs font-bold uppercase">Home Page</a>
+          <a
+            href="#"
+            class="transition-colors duration-200 bg-blue-500 hover:bg-blue-600 ml-3 rounded-full text-xs font-semibold text-white px-5 py-3 uppercase">
+            Subscribe for Updates
+          </a>
+        </div>
+      </nav>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/styles/app.css">
-    <title>My blog</title>
-</head>
+      {{ $slot }}
 
-<body>
+      <footer
+        class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
+        <img
+          src="/images/lary-newsletter-icon.png"
+          alt="Lary icon"
+          class="mx-auto"
+          style="width: 145px" />
+        <h5 class="text-3xl">Stay in touch with the latest posts</h5>
+        <p class="text-sm">Promise to keep the inbox clean. No bugs.</p>
 
-    {{ $slot }}
-
-</body>
-
+        <div class="mt-10">
+          <div
+            class="relative inline-block mx-auto lg:bg-gray-200 rounded-full">
+            <form action="#" method="POST" class="lg:flex text-sm">
+              <div class="lg:py-3 lg:px-5 flex items-center">
+                <label for="email" class="hidden lg:inline-block">
+                  <img src="/images/mailbox-icon.svg" alt="Mailbox icon" />
+                </label>
+                <input
+                  type="text"
+                  placeholder="Your.email@address.com"
+                  class="lg:bg-transparent pl-4 focus-within:outline-none"
+                  id="email"
+                  name="email" />
+              </div>
+              <button
+                type="submit"
+                class="transition-colors duration-200 bg-blue-500 hover:bg-blue-600 mt-4 lg:mt-0 lg:ml-3 rounded-full text-xs font-semibold text-white px-8 py-3 uppercase">
+                Subscribe
+              </button>
+            </form>
+          </div>
+        </div>
+      </footer>
+    </section>
+  </body>
 </html>
