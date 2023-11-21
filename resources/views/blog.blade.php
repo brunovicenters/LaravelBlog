d994edc409b101334f8ea77389f9d418d7ff393f
brunovicenters <brunovicente.dev@gmail.com>
2023-11-21 11:15:05 -0300

    Passing first post to featured

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -4,7 +4,7 @@
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         {{-- First Article --}}
-       <x-post-featured-card />
+       <x-post-featured-card :post="$posts[0]"/>
 
         {{-- Two Articles --}}
         <div class="lg:grid lg:grid-cols-2">
