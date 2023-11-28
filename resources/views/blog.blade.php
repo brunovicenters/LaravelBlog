a4ef7669b73b08bf75d61456d596ed830f5f2e1a
brunovicenters <brunovicente.dev@gmail.com>
2023-11-28 11:16:39 -0300

    Added condition in case of 0 posts

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -3,14 +3,22 @@
     @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        {{-- First Article --}}
-       <x-post-featured-card :post="$posts[0]"/>
-
-        {{-- Two Articles --}}
-        <div class="lg:grid lg:grid-cols-2">
-            @foreach ($posts->skip(1) as $post)
-                <x-post-card :post="$post" />
-            @endforeach
+        @if ($posts->count())
+            
+            {{-- First Article --}}
+            <x-post-featured-card :post="$posts[0]"/>
+            
+            @if ($posts->count()>1)
+                {{-- Two Articles --}}
+                <div class="lg:grid lg:grid-cols-2">
+                    @foreach ($posts->skip(1) as $post)
+                        <x-post-card :post="$post" />
+                    @endforeach
+                </div>
+            @endif
+        @else
+            <p class="text-center">No posts yet. Please check back later.</p>
+        @endif
     </main>
 </x-layout>
 
