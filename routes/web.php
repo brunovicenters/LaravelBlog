b967baaccfdda17cd12ce885cceb808cad970463
brunovicenters <brunovicente.dev@gmail.com>
2023-11-29 10:50:13 -0300

    Added category to / path

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('blog', ['posts' => Post::latest()->get()]);
+    return view('blog', [
+        'posts' => Post::latest()->get(),
+        'categories' => Category::all(),
+    ]);
 });
 
 
