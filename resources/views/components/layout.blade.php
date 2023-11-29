d925a8f946d159d3fe00932b7077f50a8141a4a6
brunovicenters <brunovicente.dev@gmail.com>
2023-11-29 10:58:12 -0300

    Added alpineJS

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,12 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link
       href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
-      rel="stylesheet" />
+      rel="stylesheet" 
+    />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
       href="https://fonts.googleapis.com/css2?family=Comic+Neue:wght@400;700&family=Open+Sans:wght@400;600;700&display=swap"
-      rel="stylesheet" />
+      rel="stylesheet" 
+    />
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <title>Laravel From Scratch Blog</title>
   </head>
   <body style="font-family: Open Sans, sans-serif">
