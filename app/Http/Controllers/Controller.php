fafc77a15b4ae4e46965b121adbd9451f237cfed
brunovicenters <brunovicente.dev@gmail.com>
2023-11-30 14:59:44 -0300

    Deleted wrong code

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,22 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use App\Models\Post;
-use App\Models\Category;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
-
-    public function index(){   
-
-        return view('blog', [
-            'posts' => Post::latest()->filter(request(['search']))->get(),
-            'categories' => Category::all(),
-        ]);
-    }
-
-    public function show(Post $post){
-        return view('post', ['post' => $post]);
-    }
 }
