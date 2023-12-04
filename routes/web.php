32a888f1091f9f02a921d24fa6c53c456361bcad
brunovicenters <brunovicente.dev@gmail.com>
2023-12-04 12:02:41 -0300

    Deleted old+unused block

--- routes/web.php
+++ routes/web.php
@@ -20,14 +20,6 @@ Route::get('/', [PostsController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostsController::class, 'show'])->name('post');
 
-Route::get('categories/{category:slug}', function (Category $category) {
-    return view('blog', [
-        'posts' => $category->posts,
-        'currentCategory' => $category,
-        'categories' => Category::all(),
-    ]);
-})->name('category');
-
 Route::get('authors/{author:username}', function (User $author) {
     return view('blog', [
         'posts' => $author->posts,
