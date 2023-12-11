9e43d09de2847481fa71817296e80202fcd45975
brunovicenters <brunovicente.dev@gmail.com>
2023-12-11 14:15:38 -0300

    Deleted comment

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,6 @@ use App\Models\Category;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', [PostsController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostsController::class, 'show'])->name('post');
