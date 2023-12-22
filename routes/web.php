7467a1bd0dc76809d10d7bb53affa4695a0267e7
brunovicenters <brunovicente.dev@gmail.com>
2023-12-22 12:09:19 -0300

    Added register route

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
 use App\Http\Controllers\PostsController;
+use App\Http\Controllers\RegisterController;
 use App\Models\Category;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostsController::class, 'index'])->name('home');
 
-Route::get('posts/{post:slug}', [PostsController::class, 'show'])->name('post');
+Route::get('posts/{post:slug}', [PostsController::class, 'show'])->name('post'); 
+
+Route::get('register', [RegisterController::class, 'create']);
+Route::post('register', [RegisterController::class, 'store']);
