101f01945cbe0e7747776e5f3d53257e91fc2e75
brunovicenters <brunovicente.dev@gmail.com>
2024-01-04 15:33:05 -0300

    Added error messages

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -16,7 +16,12 @@
                             type="text"
                             name="name"
                             id="name"
+                            value="{{ old('name') }}"
                             required>
+
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -28,7 +33,12 @@
                             type="text"
                             name="username"
                             id="username"
+                            value="{{ old('username') }}"
                             required>
+
+                    @error('username')
+                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -40,7 +50,12 @@
                             type="email"
                             name="email"
                             id="email"
+                            value="{{ old('email') }}"
                             required>
+
+                    @error('email')
+                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -53,6 +68,10 @@
                             name="password"
                             id="password"
                             required>
+
+                    @error('password')
+                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
