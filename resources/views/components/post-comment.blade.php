461e1a925e724bc58be7e164d92460b58eebda9f
brunovicenters <brunovicente.dev@gmail.com>
2024-01-16 14:38:29 -0300

    Added panel-component

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,20 +1,22 @@
 @props(['comment'])
 
-<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
-    <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="Profile image" width="60" height="60" class="rounded-xl">
-    </div>
-
-    <div>
-        <header class="mb-4">
-            <h3 class="font-bold">{{ $comment->author->name }}</h3>
-
-            <p class="text-xs text-gray-400 font-semibold">{{ $comment->author->username }}</p>
-            <p class="text-xs text-gray-400">Posted <time>{{ $comment->created_at->diffForHumans() }}</time></p>
-        </header>
-
-        <p>
-            {{ $comment->body }}
-        </p>
-    </div>
-</article>
+<x-panel class="bg-gray-50">
+    <article class="flex space-x-4">
+        <div class="flex-shrink-0">
+            <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="Profile image" width="60" height="60" class="rounded-xl">
+        </div>
+        
+        <div>
+            <header class="mb-4">
+                <h3 class="font-bold">{{ $comment->author->name }}</h3>
+                
+                <p class="text-xs text-gray-400 font-semibold">{{ $comment->author->username }}</p>
+                <p class="text-xs text-gray-400">Posted <time>{{ $comment->created_at->diffForHumans() }}</time></p>
+            </header>
+            
+            <p>
+                {{ $comment->body }}
+            </p>
+        </div>
+    </article>
+</x-panel>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -61,29 +61,31 @@
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
 
                     @if (auth()->id())
-                        <form action="#" method="POST" class="border border-gray-200 p-6 rounded-xl">
+                        <x-panel>
+                            <form action="#" method="POST">
 
-                            @csrf
+                                @csrf
 
-                            <header class="flex items-center space-x-4">
-                                <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" alt="Profile image" width="40" height="40" class="rounded-full">
-                                <h2 class="font-bold">What do you think about it?</h2>
-                            </header>
+                                <header class="flex items-center space-x-4">
+                                    <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" alt="Profile image" width="40" height="40" class="rounded-full">
+                                    <h2 class="font-bold">What do you think about it?</h2>
+                                </header>
 
-                            <div class="mt-4">
-                                <textarea name="body" placeholder="Write your comment..." class="w-full p-1 text-sm" rows="3"></textarea>
-                            </div>
+                                <div class="mt-4">
+                                    <textarea name="body" placeholder="Write your comment..." class="w-full p-1 text-sm" rows="3"></textarea>
+                                </div>
 
-                            <div class="flex justify-end mt-2 border-t border-gray-200 pt-6">
-                                <button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-5 rounded-2xl hover:bg-blue-600">Post</button>
-                            </div>
-                        </form>
+                                <div class="flex justify-end mt-2 border-t border-gray-200 pt-6">
+                                    <button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-5 rounded-2xl hover:bg-blue-600">Post</button>
+                                </div>
+                            </form>
+                        </x-panel>
                     @else
-                        <div class="border border-gray-200 p-6 rounded-xl">
-                            <header class="flex">
-                                <h2>Want to leave an opinion? <a href="/login" class="text-blue-500 hover:underline">Log in</a></h2>
-                            </header>
-                        </div>
+                        <x-panel>
+                                <header class="flex">
+                                    <h2>Want to leave an opinion? <a href="/login" class="text-blue-500 hover:underline">Log in</a></h2>
+                                </header>
+                        </x-panel>
                     @endif
 
                     
@@ -93,9 +95,11 @@
                             <x-post-comment :comment="$comment" />
                         @endforeach 
                     @else
-                        <article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
-                            No comments yet!
-                        </article>
+                        <x-panel>
+                            <article class="flex bg-gray-100 space-x-4">
+                                No comments yet!
+                            </article>
+                        </x-panel>
                     @endif
                 </section>
             </article>
