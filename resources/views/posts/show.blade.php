c125ce9b1847999962ff889769929051b0e2000c
brunovicenters <brunovicente.dev@gmail.com>
2024-01-16 14:30:05 -0300

    Added comment form

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -59,6 +59,35 @@
 
                 {{-- Comments --}}
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
+
+                    @if (auth()->id())
+                        <form action="#" method="POST" class="border border-gray-200 p-6 rounded-xl">
+
+                            @csrf
+
+                            <header class="flex items-center space-x-4">
+                                <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" alt="Profile image" width="40" height="40" class="rounded-full">
+                                <h2 class="font-bold">What do you think about it?</h2>
+                            </header>
+
+                            <div class="mt-4">
+                                <textarea name="body" placeholder="Write your comment..." class="w-full p-1 text-sm" rows="3"></textarea>
+                            </div>
+
+                            <div class="flex justify-end mt-2 border-t border-gray-200 pt-6">
+                                <button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-5 rounded-2xl hover:bg-blue-600">Post</button>
+                            </div>
+                        </form>
+                    @else
+                        <div class="border border-gray-200 p-6 rounded-xl">
+                            <header class="flex">
+                                <h2>Want to leave an opinion? <a href="/login" class="text-blue-500 hover:underline">Log in</a></h2>
+                            </header>
+                        </div>
+                    @endif
+
+                    
+
                     @if (count($comments) > 0)
                         @foreach ($comments as $comment) 
                             <x-post-comment :comment="$comment" />
