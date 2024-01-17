624634a40a6f16cb17992607cf31cc6f8c22278d
brunovicenters <brunovicente.dev@gmail.com>
2024-01-17 09:52:30 -0300

    Added error message

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -72,11 +72,24 @@
                                 </header>
 
                                 <div class="mt-4">
-                                    <textarea name="body" placeholder="Write your comment..." class="w-full p-1 text-sm" rows="3" required></textarea>
+                                    <textarea 
+                                        name="body" 
+                                        placeholder="Write your comment..." 
+                                        class="w-full p-1 text-sm" 
+                                        rows="3" 
+                                        required>
+                                    </textarea>
+                                    @error('body')
+                                        <span class="text-xs text-red-500">{{ $message }}</span>
+                                    @enderror
                                 </div>
 
                                 <div class="flex justify-end mt-2 border-t border-gray-200 pt-6">
-                                    <button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-5 rounded-2xl hover:bg-blue-600">Post</button>
+                                    <button 
+                                        type="submit" 
+                                        class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-5 rounded-2xl hover:bg-blue-600">
+                                        Post
+                                    </button>
                                 </div>
                             </form>
                         @else
