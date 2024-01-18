4d8a650171d08c22930fa13f7e45b883ed75db1b
bruno <brunovicente.dev@gmail.com>
2024-01-18 12:00:23 -0300

    Added newsletter subscription

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,38 @@ use App\Http\Controllers\PostsController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
+
+Route::post("newsletter", function () {
+    request()->validate(['email' => 'required|email']);
+
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us21'
+    ]);
+
+    try {
+        $response = $mailchimp->lists->addListMember("0bc7612bbd", [
+            "email_address" => request('email'),
+            "status" => "subscribed",
+        ]);
+    } catch (\Exception $e) {
+        // throw ValidationException::withMessages([
+        //     'email' => 'This email could not be added to our newsletter list.',
+        // ]);
+        return redirect('/#newsletter')
+            ->with('error', 'This email could not be added to our newsletter list.');
+    }
+
+    return redirect('/')
+        ->with('success', 'You are now signed up for our newsletter!');
+});
 
 Route::get('/', [PostsController::class, 'index'])->name('home');
 
-Route::get('posts/{post:slug}', [PostsController::class, 'show'])->name('post'); 
+Route::get('posts/{post:slug}', [PostsController::class, 'show'])->name('post');
 Route::post('posts/{post:slug}/comments/', [PostCommentsController::class, 'store'])->middleware('auth');
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
