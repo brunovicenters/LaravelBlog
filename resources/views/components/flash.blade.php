ffce5656c1f92fa8580158e977403230ab14626b
bruno <brunovicente.dev@gmail.com>
2024-01-18 11:58:53 -0300

    Added error flash message

--- resources/views/components/flash.blade.php
+++ resources/views/components/flash.blade.php
@@ -6,4 +6,12 @@
     >
     <p>{{ session('success') }}</p>
     </div>
+@elseif (session()->has('error'))
+    <div x-data="{show: true}"
+        x-init="setTimeout(() => show = false, 3000)"
+        x-show="show"
+        class="fixed bottom-3 right-3 bg-red-500 text-white py-2 px-4 rounded-xl text-sm"
+    >
+        <p>{{ session('error') }}</p>
+    </div>
 @endif
