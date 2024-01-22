bc7339038512c366f2846cdaac41762ada224d1f
bruno <brunovicente.dev@gmail.com>
2024-01-22 14:51:01 -0300

    Added textarea component

---
+++ resources/views/components/form/textarea.blade.php
@@ -0,0 +1,16 @@
+@props(['name'])
+
+<div class="mb-6">
+    <label for="{{ $name }}" class="block mb-2 uppercase font-bold text-gray-700">{{ ucwords($name) }}</label>
+    <textarea
+        class="border border-gray-400 p-2 w-full"
+        name="{{ $name }}"
+        id="{{ $name }}"
+        placeholder="Post's {{ $name }}"
+        required
+    >{{ old($name) }}</textarea>
+
+    @error($name)
+        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+    @enderror
+</div>
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -17,42 +17,16 @@
 
                 <x-form.input name="thumbnail" type="file" />
 
-                <div class="mb-6">
-                    <label for="excerpt" class="block mb-2 uppercase font-bold text-gray-700">Excerpt</label>
-                    <textarea
-                        class="border border-gray-400 p-2 w-full"
-                        name="excerpt"
-                        id="excerpt"
-                        placeholder="Post's excerpt"
-                        required
-                    >{{ old('excerpt') }}</textarea>
-    
-                    @error('excerpt')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
+                <x-form.textarea name="excerpt" />
 
-                <div class="mb-6">
-                    <label for="body" class="block mb-2 uppercase font-bold text-gray-700">Body</label>
-                    <textarea
-                        class="border border-gray-400 p-2 w-full"
-                        name="body"
-                        id="body"
-                        placeholder="Post's body"
-                        required
-                    >{{ old('body') }}</textarea>
-    
-                    @error('body')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
+                <x-form.textarea name="body" />
 
                 <div class="mb-6">
                     <label for="category_id" class="block mb-2 uppercase font-bold text-gray-700">Category</label>
 
                     <select name="category_id" id="category_id">
                         @foreach (\App\Models\Category::all() as $category)
-                            <option 
+                            <option
                                 value="{{ $category->id }}"
                                 {{ old('category_id') == $category->id ? 'selected' : '' }}
                             >
