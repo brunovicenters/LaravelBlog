8bda018b27c8b449f27e6f072056fbd477b3a1ef
bruno <brunovicente.dev@gmail.com>
2024-01-24 15:14:53 -0300

    Moved methods to another controller

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -22,28 +22,4 @@ class PostsController extends Controller
     {
         return view('posts.show', ['post' => $post, 'comments' => $post->comments]);
     }
-
-    public function create()
-    {
-        return view('admin.posts.create');
-    }
-
-    public function store()
-    {
-        $attributes = request()->validate([
-            'title' => 'required',
-            'thumbnail' => 'required|image',
-            'slug' => ['required', Rule::unique('posts', 'slug')],
-            'excerpt' => 'required',
-            'body' => 'required',
-            'category_id' => ['required', Rule::exists('categories', 'id')],
-        ]);
-
-        $attributes['user_id'] = auth()->id();
-        $attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnails');
-
-        Post::create($attributes);
-
-        return redirect('/');
-    }
 }
