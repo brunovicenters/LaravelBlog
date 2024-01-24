5d384fc2a1c2267e21bb940d4cda79fc13351099
bruno <brunovicente.dev@gmail.com>
2024-01-24 15:14:40 -0300

    Added store and create methods

--- app/Http/Controllers/AdminPostController.php
+++ app/Http/Controllers/AdminPostController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Validation\Rule;
+use App\Models\Category;
 
 class AdminPostController extends Controller
 {
@@ -12,5 +15,29 @@ class AdminPostController extends Controller
         return view('admin.posts.index', [
             'posts' => Post::paginate(50)
         ]);
+    }
+
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'title' => 'required',
+            'thumbnail' => 'required|image',
+            'slug' => ['required', Rule::unique('posts', 'slug')],
+            'excerpt' => 'required',
+            'body' => 'required',
+            'category_id' => ['required', Rule::exists('categories', 'id')],
+        ]);
+
+        $attributes['user_id'] = auth()->id();
+        $attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnails');
+
+        Post::create($attributes);
+
+        return redirect('/');
     }
 }
