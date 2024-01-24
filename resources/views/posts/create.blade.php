5acc907f72f61411fcdf55f92f99dd93e2d907a1
bruno <brunovicente.dev@gmail.com>
2024-01-24 10:07:14 -0300

    Added settings component

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,47 +1,38 @@
 <x-layout>
-    <section class="py-8 max-w-lg mx-auto">
+    <x-setting heading="Publish New Post">
+        <form action="/admin/posts" method="POST" enctype="multipart/form-data">
+            @csrf
 
-        <x-panel>
+            <x-form.input name="title" />
 
-            <h1 class=" font-bold text-xl mb-4">
-                Publish New Post
-            </h1>
+            <x-form.input name="slug" />
 
+            <x-form.input name="thumbnail" type="file" />
 
-            <form action="/admin/posts" method="POST" enctype="multipart/form-data">
-                @csrf
-    
-                <x-form.input name="title" />
+            <x-form.textarea name="excerpt" />
 
-                <x-form.input name="slug" />
+            <x-form.textarea name="body" />
 
-                <x-form.input name="thumbnail" type="file" />
+            <x-form.field>
+                <x-form.label name="category" />
 
-                <x-form.textarea name="excerpt" />
+                <select name="category_id" id="category_id">
+                    @foreach (\App\Models\Category::all() as $category)
+                        <option
+                            value="{{ $category->id }}"
+                            {{ old('category_id') == $category->id ? 'selected' : '' }}
+                        >
+                            {{ ucwords($category->name) }}
+                        </option>
+                    @endforeach
+                </select>
 
-                <x-form.textarea name="body" />
+                <x-form.error name="category_id" />
+            </x-form.field>
 
-                <x-form.field>
-                    <x-form.label name="category" />
-
-                    <select name="category_id" id="category_id">
-                        @foreach (\App\Models\Category::all() as $category)
-                            <option
-                                value="{{ $category->id }}"
-                                {{ old('category_id') == $category->id ? 'selected' : '' }}
-                            >
-                                {{ ucwords($category->name) }}
-                            </option>
-                        @endforeach
-                    </select>
-
-                    <x-form.error name="category_id" />
-                </x-form.field>
-    
-                <x-form.field>
-                    <x-form.button>Publish</x-form.button>
-                </x-form.field>
-            </form>
-        </x-panel>
-    </section>
+            <x-form.field>
+                <x-form.button>Publish</x-form.button>
+            </x-form.field>
+        </form>
+    </x-setting>
 </x-layout>
