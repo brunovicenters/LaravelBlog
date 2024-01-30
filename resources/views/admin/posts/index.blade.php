648fe230ae9fe63164e1b50da4ca1d99a0beaea7
bruno <brunovicente.dev@gmail.com>
2024-01-30 11:17:03 -0300

    Fixed table overflow

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,8 +1,8 @@
 <x-layout>
     <x-setting heading="Manage Posts">
         <div class="flex flex-col max-w-2xl ">
-            <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div class="-my-2 sm:-mx-6 lg:-mx-8">
+                <div class="py-2 align-middle inline-block min-w-full sm:px-6">
                     <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                         <table class="min-w-full divide-y divide-gray-200">
                             <thead class="bg-gray-50">
